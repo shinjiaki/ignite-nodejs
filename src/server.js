9f4129db7bc63724c794e5bdd5cfd12f42aa9a7d
shinjiaki <julio_shinji@hotmail.com>
2025-03-24 17:25:34 -0300

    Aula 2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
-  return res.end("Hello World 2");
+  const { method, url } = req;
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários');
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário');
+  }
+
+  return res.end('Hello World 2');
 });
 
 server.listen(3333);
