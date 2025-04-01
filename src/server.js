faca34e8f61933119b9d9de19d93a5b661f62e97
shinjiaki <julio_shinji@hotmail.com>
2025-04-01 12:33:51 -0300

    Aula 5.1 - Separando rotas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "node:http";
-import { randomUUID } from "node:crypto"
 import { json } from "./middlewares/json.js";
-import { Database } from "./database.js";
+import { routes } from "./routes.js";
 
 // UUID => Unique Universal ID
 
@@ -32,31 +31,18 @@ import { Database } from "./database.js";
 
 // HTTP Status Code
 
-const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-    return res.end(JSON.stringify(users));
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end('Criação de usuário');
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end();
