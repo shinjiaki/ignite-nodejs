896fc1c9507a559c4d8255b546af959be19c1887
shinjiaki <julio_shinji@hotmail.com>
2025-04-01 12:20:02 -0300

    Aula 4.2 - Banco de dados e persistência

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
 
 // - Criar usuários
 // - Listagem usuários
@@ -28,7 +29,7 @@ import { json } from "./middlewares/json.js";
 
 // HTTP Status Code
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -36,18 +37,21 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users));
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end('Criação de usuário');
   }
